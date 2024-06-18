cdd634db5a636f5107ea3ef879f1f7ebf189b63d
nameoftheuser1 <103398819+nameoftheuser1@users.noreply.github.com>
2024-06-18 14:03:55 +0800

    first commit

---
+++ src/main/java/com/nyadesu/se/shikonokokoshitantan/ShikonokokoshitantanApplication.java
@@ -0,0 +1,13 @@
+package com.nyadesu.se.shikonokokoshitantan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShikonokokoshitantanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShikonokokoshitantanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyadesu/se/shikonokokoshitantan/ShikonokokoshitantanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyadesu.se.shikonokokoshitantan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShikonokokoshitantanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
