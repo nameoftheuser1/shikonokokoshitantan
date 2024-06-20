6a31ff6d86280cd0c3ac2aa8326d929ae76902a5
nameoftheuser1 <103398819+nameoftheuser1@users.noreply.github.com>
2024-06-20 15:32:02 +0800

    Activity - 2

---
+++ README.md
@@ -0,0 +1,7 @@
+![alt text](notimportant\picture.jpg)
+![pogi](notimportant\pircture2.jpg)
+### This is my project inspired by these people. ♥♥
+
+`
+kung ikaw ay masaya edi tumawa ka --haha
+`
--- src/main/java/com/nyadesu/se/shikonokokoshitantan/controllers/UserController.java
+++ src/main/java/com/nyadesu/se/shikonokokoshitantan/controllers/UserController.java
@@ -1,14 +1,40 @@
 package com.nyadesu.se.shikonokokoshitantan.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nyadesu.se.shikonokokoshitantan.model.UserModel;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class UserController {
     @GetMapping("/user")
-    public UserModel getUser(){
+    public UserModel getUser() {
         return new UserModel(11, "koshino", "koshino@gmail.com", "12345");
+    }
+
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+
+    @GetMapping("/users")
+    public List<UserModel> getUsers() {
+        List<UserModel> users = new ArrayList<>();
+        users.add(new UserModel(1, "mario", "mario@gmail.com", "1234"));
+        users.add(new UserModel(2, "leo", "leo@gmail.com", "1234"));
+        users.add(new UserModel(3, "jon", "jon@gmail.com", "1234"));
+        users.add(new UserModel(4, "mike", "mike@gmail.com", "1234"));
+
+        return users;
+    }
+
+    @GetMapping("/user/{name}")
+    public UserModel getUserFromName(@PathVariable("name") String name) {
+        return new UserModel(1, "mario", "mario@gmail.com", "1234");
     }
 }
